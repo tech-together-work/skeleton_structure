1570a3012cc79e8979e11c82b7a50f9e6aa447a4
Parasgr7 <parasgr484@gmail.com>
2024-10-21 00:51:52 +0530

    decrement counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { useDispatch, useSelector } from "react-redux";
-import styles from "./App.module.scss";
-import { RootState } from "./store/store";
-import { increment } from "./store/counterSlice";
+import { useDispatch, useSelector } from 'react-redux';
+import styles from './App.module.scss';
+import { RootState } from './store/store';
+import { increment, decrement } from './store/counterSlice';
 
 function App() {
   //Redux usage
@@ -11,12 +11,16 @@ function App() {
   const increaseCounter = () => {
     dispatch(increment());
   };
+  const decreaseCounter = () => {
+    dispatch(decrement());
+  };
 
   return (
     <div className={styles.bg}>
       <h2>Swagatam!!!</h2>
-      <button onClick={increaseCounter}>Click me</button>
+      <button onClick={increaseCounter}>Add</button>
       <h3> {counterValue.value}</h3>
+      <button onClick={decreaseCounter}>Subtract</button>
     </div>
   );
 }
