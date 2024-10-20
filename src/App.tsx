1cdd9e55d37afb9979f4c3f82bd81ed284252c01
Shubham Prakash <shub.prakash9001@gmail.com>
2024-10-21 02:19:05 +0530

    feat: added sus

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import routes from '~react-pages';
 import PageLayout from './components/shared/PageLayout/PageLayout';
+import { Suspense } from 'react';
 
 const router = createBrowserRouter(routes);
 
 const App = () => {
   return (
-    <PageLayout>
-      <RouterProvider router={router} />;
-    </PageLayout>
+    <Suspense fallback={'Loading...'}>
+      <PageLayout>
+        <RouterProvider router={router} />;
+      </PageLayout>
+    </Suspense>
   );
 };
 
