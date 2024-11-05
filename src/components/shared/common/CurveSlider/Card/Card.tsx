0b88233cf386bfc6f47041bf72d6a8436621c13e
Shubham Prakash <shub.prakash9001@gmail.com>
2024-11-05 20:52:00 +0530

    feat: added slider

--- src/components/shared/common/CurveSlider/Card/Card.tsx
+++ src/components/shared/common/CurveSlider/Card/Card.tsx
@@ -1,16 +1,28 @@
-import { H6 } from '../../../Text/Text';
+import { H3, H6 } from '../../../Text/Text';
 import styles from './Card.module.scss';
 import dummyImage from '../../../../../assets/images/banner.png';
 import { Link } from 'react-router-dom';
+import clsx from 'clsx';
 
-const Card = () => {
+interface Props {
+  isActive?: boolean;
+}
+const Card: React.FC<Props> = ({ isActive }) => {
   return (
-    <Link to={''}>
-      <div className={styles.card}>
+    <Link to={''} className={clsx(isActive && styles.activeLink)}>
+      <div className={clsx(styles.card, isActive && styles.active)}>
         <div className={styles.imageWrap}>
+          <div className={styles.grad}></div>
           <img className={styles.image} alt="image" src={dummyImage} />
+          <div className={styles.subText}>
+            <H6>
+              Enjoy Swagatam’s Luxury group tours with your friends or family
+            </H6>
+          </div>
         </div>
-        <H6 fontWeight="semibold">Family Tour</H6>
+      </div>
+      <div className={styles.bottomText}>
+        <H3 fontWeight="semibold">Family Tour</H3>
       </div>
     </Link>
   );
--- src/components/shared/common/CurveSlider/CurveSlider.tsx
+++ src/components/shared/common/CurveSlider/CurveSlider.tsx
@@ -1,5 +1,5 @@
 // Import React and Swiper
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
 import styles from './CurveSlider.module.scss';
 // Import Swiper styles
 import 'swiper/css';
@@ -11,38 +11,51 @@ import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 import 'swiper/css/navigation';
 import Card from './Card/Card';
 import SwiperNav from '../SwiperNav/SwiperNav';
+import { useEffect, useState } from 'react';
 
 const CurveSlider = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const arr = ['1', '2', '3', '4'];
+  console.log(activeIndex);
+
+  useEffect(() => {}, []);
   return (
     <div className={styles.curveSliderContainer}>
       <Swiper
         className={styles.swiper}
         modules={[Navigation, Pagination]}
         spaceBetween={'24px'}
-        slidesPerView={'auto'}
+        breakpoints={{
+          200: {
+            slidesPerView: 2, // Mobile
+            spaceBetween: 0,
+          },
+          768: {
+            slidesPerView: 2, // Tablet
+            spaceBetween: 20,
+          },
+          1024: {
+            slidesPerView: 3, // Desktop
+            spaceBetween: 24,
+          },
+        }}
+        wrapperClass={'curveWrapper'}
+        loop
+        centeredSlides
         pagination={{ clickable: true }}
         scrollbar={{ draggable: true }}
         onSwiper={(swiper) => console.log(swiper)}
-        onSlideChange={() => console.log('slide change')}
+        onSlideChange={(swiper) => {
+          setActiveIndex(Math.floor(swiper.realIndex % arr.length));
+        }}
       >
-        <SwiperSlide className={styles.slider}>
-          <Card />
-        </SwiperSlide>
-        <SwiperSlide className={styles.slider}>
-          <Card />
-        </SwiperSlide>
-        <SwiperSlide className={styles.slider}>
-          <Card />
-        </SwiperSlide>
-        <SwiperSlide className={styles.slider}>
-          <Card />
-        </SwiperSlide>
-        <SwiperSlide className={styles.slider}>
-          <Card />
-        </SwiperSlide>
-        <SwiperSlide className={styles.slider}>
-          <Card />
-        </SwiperSlide>
+        {arr.map((item, index) => {
+          return (
+            <SwiperSlide key={index} className={styles.slider}>
+              <Card isActive={index === activeIndex} />
+            </SwiperSlide>
+          );
+        })}
         <SwiperNav />
       </Swiper>
     </div>
